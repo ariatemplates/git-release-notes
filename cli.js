a4a5a7931771f00ead1c53e586ae1035a9175365
Fabio Crisci <piuccio@gmail.com>
2018-12-27 11:05:01 +0900

    Update dependencies and remove support for node 4 (#50)
    Migrate from deprecated optimist to yargs

--- cli.js
+++ cli.js
@@ -1,54 +1,57 @@
 #!/usr/bin/env node
-var argv = require("optimist").usage("git-release-notes [<options>] <since>..<until> <template>")
-.options("f", {
-	"alias": "file"
-})
-.options("p", {
-	"alias": "path",
-	"default": process.cwd()
-})
-.options("t", {
-	"alias": "title",
-	"default": "(.*)"
-})
-.boolean("i")
-.alias("i", "ignore-case")
-.options("m", {
-	"alias": "meaning",
-	"default": ['type']
-})
-.options("b", {
-	"alias": "branch",
-	"default": "master"
-})
-.options("s", {
-	"alias": "script"
-})
-.options("o", {
-	"alias": "gitlog-option",
-	"default" : []
-})
-.boolean("c")
-.alias("c", "merge-commits")
-.describe({
-	"f": "Configuration file",
-	"p": "Git project path",
-	"t": "Commit title regular expression",
-	"i": "Ignore case of title's regular expression",
-	"m": "Meaning of capturing block in title's regular expression",
-	"b": "Git branch, defaults to master",
-	"s": "External script to rewrite the commit history",
-	"c": "Only use merge commits",
-	"o": "Additional git log options AND ignore 'c' option"
-})
-.boolean("version")
-.check(function (argv) {
-	if (argv._.length == 2) {
-		return true;
-	}
-	throw "Invalid parameters, please specify an interval and the template";
-})
-.argv;
+var argv = require('yargs')
+	.usage('git-release-notes [<options>] <since>..<until> <template>')
+	.demandCommand(2)
+	.example(
+		'git-release-notes -t "(feat|bug): (.*)" -m type -m title v1.0.0..v2.0.0 markdown',
+		'Match all commits with starting with either `feat:` or `bug` between the tags `v1.0.0` and `v2.0.0`'
+	)
+	.wrap(null)
+	.option('f', {
+		alias: 'file',
+		describe: 'Configuration file. Use it instead of passing command line options'
+	})
+	.option('p', {
+		alias: 'path',
+		describe: 'Git project path. Defaults to the current path',
+		default: process.cwd(),
+	})
+	.option('t', {
+		alias: 'title',
+		describe: 'Commit title regular expression',
+		default: '(.*)',
+	})
+	.option('i', {
+		alias: 'ignore-case',
+		describe: 'Ignore case of title\'s regular expression',
+		type: 'boolean',
+	})
+	.option('m', {
+		alias: 'meaning',
+		describe: 'Meaning of capturing block in title\'s regular expression',
+		default: ['type'],
+	})
+	.option('b', {
+		alias: 'branch',
+		describe: 'Git branch, defaults to master',
+		default: 'master',
+		type: 'string',
+	})
+	.option('s', {
+		alias: 'script',
+		describe: 'External script to rewrite the commit history',
+	})
+	.option('o', {
+		alias: 'gitlog-option',
+		describe: 'Additional git log options AND ignore \'c\' option',
+		default: [],
+	})
+	.option('c', {
+		alias: 'merge-commits',
+		describe: 'Only use merge commits',
+		type: 'boolean',
+	})
+	.argv;
 
 const index = require('./index');
 index(argv, argv._[0], argv._[1])
